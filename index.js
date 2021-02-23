c05034cf337c0f539e5797c56801a965b377786f
james gill <jimmygill28@hotmail.com>
2021-02-23 21:57:21 +0000

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const apiRouter = require("./routes/api.router");
 const {
   customErrorHandler,
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use("/api", apiRouter);
 
 app.all("/*", (req, res, next) => {
-  res.status(404).send({ msg: "404 Error: Path Not found" });
+  // res.status(404).send({ msg: "404 Error: Path Not found" });
+  next({ status: 404, msg: "Route not found" });
 });
 
 app.use(customErrorHandler);
