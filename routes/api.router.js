dcf32fbed63aecdff177303cd96b5956dfef673f
james gill <jimmygill28@hotmail.com>
2021-02-24 17:48:59 +0000

    fix db problem

--- routes/api.router.js
+++ routes/api.router.js
@@ -4,7 +4,7 @@ const answersRouter = require("./answers.router");
 
 const { handle405Errors } = require("../error_handlers/index");
 
-// apiRouter.all("/", handle405Errors);
+apiRouter.all("/", handle405Errors);
 
 apiRouter.use("/questions", questionsRouter);
 apiRouter.use("/answers", answersRouter);
