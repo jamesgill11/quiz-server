2d65e22f43e5aed33efc83914b83e8edda9ac442
james gill <jimmygill28@hotmail.com>
2021-02-25 14:32:07 +0000

    trying to append ?ssl=true

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,9 @@
 const ENV = process.env.NODE_ENV || "production";
 const knex = require("knex");
 
-process.env.DATABASE_URL =
-  "postgres://mjlsjmeybblkct:5daef197e49132f9a9e440af7dbadadc9324a10535f2fb7cc5fb43b8d7eee834@ec2-52-70-67-123.compute-1.amazonaws.com:5432/d3tgun6ovoc5dm";
-
 const dbConfig =
   ENV === "production"
-    ? { client: "pg", connection: process.env.DATABASE_URL }
+    ? { client: "pg", connection: process.env.DATABASE_URL?ssl=true }
     : require("../knexfile");
 console.log(process.env.DATABASE_URL);
 module.exports = knex(dbConfig);
