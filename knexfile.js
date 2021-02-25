15b381445f9b92cdbfc3fce174000309a65a8ccb
james gill <jimmygill28@hotmail.com>
2021-02-25 10:20:33 +0000

    changed connectionString

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,8 @@ const customConfig = {
   },
   production: {
     connection: {
-      connectionString: DB_URL,
+      connectionString:
+        "postgres://mjlsjmeybblkct:5daef197e49132f9a9e440af7dbadadc9324a10535f2fb7cc5fb43b8d7eee834@ec2-52-70-67-123.compute-1.amazonaws.com:5432/d3tgun6ovoc5dm",
       ssl: {
         rejectUnauthorized: false,
       },
