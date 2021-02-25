9fb982af09cd1718f0440b718871795a87b7dbf5
james gill <jimmygill28@hotmail.com>
2021-02-25 14:34:27 +0000

    append

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
 const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
 
+process.env.DATABASE_URL =
+  "postgres://mjlsjmeybblkct:5daef197e49132f9a9e440af7dbadadc9324a10535f2fb7cc5fb43b8d7eee834@ec2-52-70-67-123.compute-1.amazonaws.com:5432/d3tgun6ovoc5dm?ssl=true";
+
 const dbConfig =
   ENV === "production"
-    ? { client: "pg", connection: process.env.DATABASE_URL?ssl=true }
+    ? { client: "pg", connection: process.env.DATABASE_URL }
     : require("../knexfile");
 console.log(process.env.DATABASE_URL);
 module.exports = knex(dbConfig);
