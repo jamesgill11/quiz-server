6d50cf1676e2645950dad5b0e44e80939cb000e3
james gill <jimmygill28@hotmail.com>
2021-07-19 17:10:29 +0100

    added new headers

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
-app.use(cors({ origin: "*" }));
 const apiRouter = require("./routes/api.router");
 const {
   customErrorHandler,
@@ -12,6 +11,7 @@ const {
   handleServerErrors,
 } = require("./error_handlers/index");
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -24,6 +24,50 @@ app.use("/", (req, res, next) => {
 app.use("*", (req, res, next) => {
   // res.status(404).send({ msg: "404 Error: Path Not found" });
   next({ status: 404, msg: "Route not found" });
+});
+
+app.all("*", function (req, res, next) {
+  /**
+   * Response settings
+   * @type {Object}
+   */
+  var responseSettings = {
+    AccessControlAllowOrigin: req.headers.origin,
+    AccessControlAllowHeaders:
+      "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+    AccessControlAllowMethods: "POST, GET, PUT, DELETE, OPTIONS",
+    AccessControlAllowCredentials: true,
+  };
+
+  /**
+   * Headers
+   */
+  res.header(
+    "Access-Control-Allow-Credentials",
+    responseSettings.AccessControlAllowCredentials
+  );
+  res.header(
+    "Access-Control-Allow-Origin",
+    responseSettings.AccessControlAllowOrigin
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    req.headers["access-control-request-headers"]
+      ? req.headers["access-control-request-headers"]
+      : "x-requested-with"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    req.headers["access-control-request-method"]
+      ? req.headers["access-control-request-method"]
+      : responseSettings.AccessControlAllowMethods
+  );
+
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.use(customErrorHandler);
--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -29,17 +29,6 @@ exports.authUser = (req, res, next) => {
         secure: true,
       });
 
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-      ); // If needed
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-      ); // If needed
-      res.setHeader("Access-Control-Allow-Credentials", true); // If needed
-
       res.send(tokens);
     })
     .catch((error) => {
