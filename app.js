b658c4e55c961462f3920acfb69b2fb91421039f
james gill <jimmygill28@hotmail.com>
2021-07-19 15:01:51 +0100

    deleted cors

--- app.js
+++ app.js
@@ -15,8 +15,15 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
