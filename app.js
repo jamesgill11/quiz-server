4a20abe20c68dd10da4e18558552770ee585ad4f
james gill <jimmygill28@hotmail.com>
2021-07-19 13:51:03 +0100

    edit

--- app.js
+++ app.js
@@ -15,17 +15,19 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.options("/api/login", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST");
-  res.header("Access-Control-Allow-Headers", "accept, content-type");
-  res.header("Access-Control-Max-Age", "1728000");
-  return res.sendStatus(200);
-});
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api", apiRouter);
 
