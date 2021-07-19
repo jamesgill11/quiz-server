87ee8453049b1757caef23e0012cadb176444634
james gill <jimmygill28@hotmail.com>
2021-07-19 15:11:03 +0100

    added cors package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
+app.use(cors());
 const apiRouter = require("./routes/api.router");
 const {
   customErrorHandler,
@@ -10,20 +11,7 @@ const {
   handle405Errors,
   handleServerErrors,
 } = require("./error_handlers/index");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-// app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
