74303c405fcc653d1e1b35c1b2c6f55731c1066d
james gill <jimmygill28@hotmail.com>
2021-07-19 13:53:36 +0100

    edit

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://my-quiz-server.herokuapp.com"
+  ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
