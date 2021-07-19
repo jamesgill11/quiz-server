cb03ff214991f113ae215c21f14498dccf913f4a
james gill <jimmygill28@hotmail.com>
2021-07-19 12:58:27 +0100

    added headers

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api", cors(corsOptions), apiRouter);
 
 app.use("/", (req, res, next) => {
