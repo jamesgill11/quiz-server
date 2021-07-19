c9bab46f6795c7913aba84c74adac298277ffa1d
james gill <jimmygill28@hotmail.com>
2021-07-19 12:24:36 +0100

    cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", cors(), apiRouter);
+app.use("/api", apiRouter);
 
 app.use("/", (req, res, next) => {
   res.send({ status: 200, msg: "Welcome to my Quiz" });
--- routes/api.router.js
+++ routes/api.router.js
@@ -8,6 +8,7 @@ const { loginRouter, rfTokenRouter } = require("./login.router");
 
 const { handle405Errors } = require("../error_handlers/index");
 const { authenticateToken } = require("../middleware/authorization");
+const cors = require("cors");
 
 // apiRouter.all("/", handle405Errors);
 
@@ -15,7 +16,7 @@ apiRouter.use("/questions", questionsRouter);
 apiRouter.use("/answers", answersRouter);
 apiRouter.use("/register", registerRouter);
 apiRouter.use("/correct", correctRouter);
-apiRouter.use("/newq", authenticateToken, newqRouter);
+apiRouter.use("/newq", authenticateToken, cors, newqRouter);
 apiRouter.use("/login", loginRouter);
 apiRouter.use("/refresh_token", rfTokenRouter);
 
