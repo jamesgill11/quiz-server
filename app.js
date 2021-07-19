2285ad21277ca6d498670f2b23a3f444883130ca
james gill <jimmygill28@hotmail.com>
2021-07-19 13:33:51 +0100

    edit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.options("/url...", function (req, res, next) {
+app.options("/api", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "POST");
   res.header("Access-Control-Allow-Headers", "accept, content-type");
