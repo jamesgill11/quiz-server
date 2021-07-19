8653d0ced1d48520185f0acbc921561d8fe98818
james gill <jimmygill28@hotmail.com>
2021-07-19 13:32:19 +0100

    added app.options

--- app.js
+++ app.js
@@ -15,21 +15,19 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+app.options("/url...", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST");
+  res.header("Access-Control-Allow-Headers", "accept, content-type");
+  res.header("Access-Control-Max-Age", "1728000");
+  return res.sendStatus(200);
+});
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.use("/api", cors(corsOptions), apiRouter);
+app.use("/api", apiRouter);
 
 app.use("/", (req, res, next) => {
   res.send({ status: 200, msg: "Welcome to my Quiz" });
--- routes/api.router.js
+++ routes/api.router.js
@@ -8,20 +8,15 @@ const { loginRouter, rfTokenRouter } = require("./login.router");
 
 const { handle405Errors } = require("../error_handlers/index");
 const { authenticateToken } = require("../middleware/authorization");
-const cors = require("cors");
+
 // apiRouter.all("/", handle405Errors);
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 
 apiRouter.use("/questions", questionsRouter);
 apiRouter.use("/answers", answersRouter);
 apiRouter.use("/register", registerRouter);
 apiRouter.use("/correct", correctRouter);
-apiRouter.use("/newq", authenticateToken, cors(corsOptions), newqRouter);
-apiRouter.use("/login", cors(corsOptions), loginRouter);
-apiRouter.use("/refresh_token", cors(corsOptions), rfTokenRouter);
+apiRouter.use("/newq", authenticateToken, newqRouter);
+apiRouter.use("/login", loginRouter);
+apiRouter.use("/refresh_token", rfTokenRouter);
 
 module.exports = apiRouter;
