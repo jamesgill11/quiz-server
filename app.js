08a1b7d0e3afd107b17380b475b5903619469e93
james gill <jimmygill28@hotmail.com>
2021-07-20 21:12:26 +0100

    6

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ const {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(express.json());
