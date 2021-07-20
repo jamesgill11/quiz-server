43dcf9a9aac20d04f5cae6e7afe3fb4d1acdcb5e
james gill <jimmygill28@hotmail.com>
2021-07-20 21:20:05 +0100

    7

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ const {
   handleServerErrors,
 } = require("./error_handlers/index");
 
-app.use(
-  cors({
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["*"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 
