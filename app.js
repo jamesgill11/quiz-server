5255b7afbf42ddd66a6c4c2ab7fe83a6d695fba7
james gill <jimmygill28@hotmail.com>
2021-07-20 14:59:29 +0100

    3

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const {
 // app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-
+  res.header("Access-Control-Allow-Headers: Content-Type");
   next();
 });
 app.use(express.json());
