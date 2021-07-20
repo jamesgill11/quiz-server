94e9ca51a95e66e70cee3ffa6f70ab708034d02f
james gill <jimmygill28@hotmail.com>
2021-07-20 14:45:54 +0100

    more headers

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const {
 // app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 app.use(express.json());
