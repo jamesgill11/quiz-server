0df15af750f208e50e16cb84fdeb759405c1ad53
james gill <jimmygill28@hotmail.com>
2021-07-20 20:50:20 +0100

    6

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const {
   handleServerErrors,
 } = require("./error_handlers/index");
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
