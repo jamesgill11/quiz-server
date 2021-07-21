0acbfc2baf2bc75c9a349e5edd00e3cefad397d9
james gill <jimmygill28@hotmail.com>
2021-07-21 21:35:19 +0100

    28

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
@@ -10,7 +10,7 @@ const {
   handle405Errors,
   handleServerErrors,
 } = require("./error_handlers/index");
-const cors = require("./middleware/cors");
+// const cors = require("./middleware/cors");
 
 app.use(
   cors()
