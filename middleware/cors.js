9d75a4573664326e876166c32b65429ea6c5d345
james gill <jimmygill28@hotmail.com>
2021-07-21 19:26:22 +0100

    15

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   return function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,  Authorization");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
   };
 };
