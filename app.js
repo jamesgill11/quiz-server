61bc5e359d8c03d206f6e6b7bb02188926d70184
james gill <jimmygill28@hotmail.com>
2021-07-21 19:21:06 +0100

    13

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ const {
 } = require("./error_handlers/index");
 const cors = require("./middleware/cors");
 
-// app.use(
-//   cors({
-//     origin: ["*"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
@@ -31,7 +23,6 @@ app.use("/", (req, res, next) => {
 });
 
 app.use("*", (req, res, next) => {
-  // res.status(404).send({ msg: "404 Error: Path Not found" });
   next({ status: 404, msg: "Route not found" });
 });
 
