bb29d020299570eb86686d4f955b2964eebcbb94
james gill <jimmygill28@hotmail.com>
2021-07-21 19:05:36 +0100

    11

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use("/api", apiRouter);
 
 app.use("/", (req, res, next) => {
