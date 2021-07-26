2faf14e043b4f0256a73d2872c03bcd614f143c2
james gill <jimmygill28@hotmail.com>
2021-07-26 17:22:10 +0100

    48

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const {
 // );
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
