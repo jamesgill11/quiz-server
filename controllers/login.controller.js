31237a267bdd580ea2ef0633c76258fd4fa5c6a0
james gill <jimmygill28@hotmail.com>
2021-07-26 16:09:23 +0100

    36

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -28,7 +28,13 @@ exports.authUser = (req, res, next) => {
         sameSite: "none",
         secure: true,
       });
-      console.log(req, res);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Credentials", true);
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, GET, PUT, DELETE, OPTIONS"
+      );
+      res.header("Access-Control-Allow-Headers", "Content-Type");
       res.send(tokens);
     })
     .catch((error) => {
--- middleware/authorization.js
+++ middleware/authorization.js
@@ -13,6 +13,7 @@ const authenticateToken = (req, res, next) => {
       return res.status(403).send({ error: error.message });
     }
     req.user = user;
+
     next();
   });
 };
