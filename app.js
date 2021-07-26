a67399ff51f834e5328cf695fd0fb9c75761c054
james gill <jimmygill28@hotmail.com>
2021-07-26 17:12:18 +0100

    46

--- app.js
+++ app.js
@@ -20,12 +20,7 @@ const {
 //   })
 // );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
