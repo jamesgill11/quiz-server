e7c1ac1f9d640ab886f6169a82ad85d3e5ed7051
james gill <jimmygill28@hotmail.com>
2021-07-26 17:51:49 +0100

    53

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    allowedHeaders: ["sessionId", "Content-Type"],
-    exposedHeaders: ["sessionId"],
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
+    origin: true,
   })
 );
 const cookieParser = require("cookie-parser");
