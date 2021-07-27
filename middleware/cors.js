49d48841d060da705039b3b27e6ae284ed46bd73
james gill <jimmygill28@hotmail.com>
2021-07-27 11:09:29 +0100

    69

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,6 +3,7 @@ module.exports = function () {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 
     // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     // res.header("Access-Control-Allow-Headers", "Content-Type");
