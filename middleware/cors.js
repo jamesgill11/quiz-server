830ade888350b8cbf3d2a097020e640500b03024
james gill <jimmygill28@hotmail.com>
2021-07-27 11:11:29 +0100

    70

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,12 +1,20 @@
 module.exports = function () {
-  return function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-
-    // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  return function (req, response, next) {
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     // res.header("Access-Control-Allow-Headers", "Content-Type");
+    // res.header("Access-Control-Allow-Credentials", "true");
+    // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,OPTIONS,POST,PUT"
+    );
+    response.setHeader(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+
     next();
   };
 };
