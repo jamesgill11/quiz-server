5f622cabf050c8408f072699b1ceda4114477bf7
james gill <jimmygill28@hotmail.com>
2021-07-27 15:27:25 +0100

    73

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ const crossOrigin = require("./middleware/cors");
 
 //
 
-app.use((req, res, next) => {
+app.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type"),
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Content-Length, Authorization, Accept,X-Requested-With"
+  ),
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   next();
 });
 app.use(express.json());
