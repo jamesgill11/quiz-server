2e1f29fec9da5f8b735a03c6c403affec09284c5
james gill <jimmygill28@hotmail.com>
2021-07-28 15:48:37 +0100

    96

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ const corsOptions = { credentials: true, origin: "http://localhost:3000" };
 // });
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With, Accept"
-  );
+  // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Content-Type, Authorization, Content-Length, X-Requested-With, Accept"
+  // );
   next();
 });
 
