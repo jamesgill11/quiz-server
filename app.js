214eea3957a313741439c0c66df044e0a17f9969
james gill <jimmygill28@hotmail.com>
2021-07-28 15:20:26 +0100

    93

--- app.js
+++ app.js
@@ -25,8 +25,14 @@ const corsOptions = { credentials: true, origin: "http://localhost:3000" };
 //   );
 //   next();
 // });
-app.use(cors(corsOptions));
-app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, Accept"
+  );
+});
 
 app.use(express.json());
 app.use(cookieParser());
