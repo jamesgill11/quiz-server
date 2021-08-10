f202eddd22ddfa102b7fe7fea5c668dd43f4851e
james gill <jimmygill28@hotmail.com>
2021-08-10 14:51:44 +0100

    added fix authenicate

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -8,7 +8,7 @@ const authenticateToken = (req, res, next) => {
   if (token === null) {
     return res.status(401).send({ error: "Null Token" });
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+  jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`, (error, user) => {
     if (error) {
       return res.status(403).send({ error: error.message });
     }
